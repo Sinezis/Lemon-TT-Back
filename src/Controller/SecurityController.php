2f9d8db2c3767ad3980e4266cd4fa2db1b83ce03
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2024-03-28 22:54:41 +0100

    created HomePage and redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,15 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private Security $security;
+
+    public function __construct(
+        Security $security
+    ) {
+        $this->security = $security;
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    {   
+        // Redirect to Home connected Users
+        if ($this->security->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
