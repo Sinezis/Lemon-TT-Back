25a5e9723c47ded77b9b9f2bb10d36374a15d6e6
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2024-03-29 22:18:36 +0100

    created access to my event list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Service\MyListService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    private $security;
+
+    public function __construct(
+        Security $security
+    ) {
+        $this->security = $security;
+    }
+
     #[Route(path: '/home', name: 'app_home')]
     public function home(
         Request $request,
@@ -21,6 +31,22 @@ class HomeController extends AbstractController
         $events = $eventRepo->findUpcoming();
         return $this->render('home/index.html.twig', [
             'events'=>$events
+        ]);
+    }
+
+    #[Route(path: '/mylist', name: 'my_list')]
+    public function myList(
+        Request $request,
+        MyListService $service,
+    ): Response
+    {
+        $user = $this->security->getUser();
+        $eventsCreated = $service->getUserEvents($user);
+        $eventsAttending = $user->getAttending();
+
+        return $this->render('mylist/list.html.twig', [
+            'eventsAttending' => $eventsAttending,
+            'eventsCreated' => $eventsCreated
         ]);
     }
 }
---
+++ src/Service/Event/MyListService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Event;
+use App\Entity\User;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
+
+class MyListService {
+    private $eventRepo;
+
+    public function __construct(
+        EventRepository $eventRepo
+    ) {
+        $this->eventRepo = $eventRepo;
+    }
+
+    public function getUserEvents(
+        User $user
+    ) {
+        $events = $this->eventRepo->findByCreatedBy($user);
+
+        return $events;
+    }
+
+    public function update(
+        FormInterface $form,
+        User $user,
+        Event $event
+    ) {
+        $event = $form->getData();
+
+        try {
+            $this->entityManager->persist($event);
+            $this->entityManager->flush();
+        } catch (Exception $e) {
+            throw new Exception("L'événement n'a pas pu être sauvegardé. Veuillez réessayer.");
+        }
+
+        return true;
+    }
+
+    public function delete(
+        Event $event
+    ) {
+        try {
+            $this->entityManager->remove($event);
+            $this->entityManager->flush();
+        } catch (Exception $e) {
+            throw new Exception("L'événement n'a pas pu être supprimé. Veuillez réessayer.");
+        }
+
+        return true;
+    }
+}
